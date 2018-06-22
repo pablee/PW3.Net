2d9833a73aa4f923cc15b61698a10cb6c793e8ef
pablee <tcpipablo@gmail.com>
2018-06-22 11:15:00 -0300

    password ok

---
+++ Login/WebApplication1/Controllers/LoginController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult IniciarSesion()
+        {           
+            string correo = Request.Form["correo"];
+            string password = Request.Form["password"];
+
+            Usuario User = new Usuario();
+            User.Correo = correo;
+            User.Password = password;
+
+            ViewBag.User = User;
+
+            if(User.IniciarSesion(User)==true)
+            {
+                return View("Usuario");
+            }
+            else
+            {
+                return View("Index");
+            }
+
+            /*
+            ViewData["Username"]= username;
+            ViewData["Password"] = password;
+            return View("Usuario");
+            */
+        }
+
+
+        public ActionResult Registrarse()
+        {
+            string nombre = Request.Form["nombre"];
+            string apellido = Request.Form["apellido"];
+            string correo = Request.Form["correo"];
+            string password = Request.Form["password"];
+
+            Usuario User = new Usuario();
+            User.Nombre = nombre;
+            User.Apellido = apellido;
+            User.Correo = correo;
+            User.Password = password;
+       
+            ViewBag.User = User;
+
+            if (User.Registrarse(User) == true)
+            {
+                return View("Usuario");
+            }
+            else
+            {
+                return View("Index");
+            }
+
+            /*
+            ViewData["Username"]= username;
+            ViewData["Password"] = password;
+            return View("Usuario");
+            */
+        }
+    }
+}
---
+++ Login/WebApplication1/Models/Usuario.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Usuario
+    {
+        public string Correo { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Password { get; set; }
+        public List<Usuario> usuariosRegistrados = new List<Usuario>();        
+
+        public Usuario()
+        {
+            
+        }
+
+        public Boolean IniciarSesion(Usuario User)
+        {
+            if (User.Correo=="Pablo")
+            {
+                if(User.Password=="12345")
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public Boolean Registrarse(Usuario User)
+        {
+            usuariosRegistrados.Add(User);
+            return true;
+        }
+    }
+}
